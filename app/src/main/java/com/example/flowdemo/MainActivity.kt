cc67dd63070c227280ccde7c24bb2bf5b1f7b17f
zashine <zx_1124@foxmail.com>
2025-02-19 21:38:01 +0700

    clean code

--- app/src/main/java/com/example/flowdemo/MainActivity.kt
+++ app/src/main/java/com/example/flowdemo/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.flowdemo.ui.theme.FlowDemoTheme
 import com.example.flowdemo.ui.viewmodel.MainViewModel
@@ -42,19 +40,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    FlowDemoTheme {
-//        Greeting("Android")
-//    }
-//}
